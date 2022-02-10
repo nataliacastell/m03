94a2a73e52476192bdad9dcc1636190479c77863
nataliacastell <94447965+nataliacastell@users.noreply.github.com>
2022-02-10 09:49:08 +0100

    Java6

--- array_ncastell/array_buscar_el_numero.java
+++ array_ncastell/array_buscar_el_numero.java
@@ -1,28 +1,31 @@
+package array_ncastell;
+
 import java.util.Scanner;
 
-public class Main{
+public class array_buscar_el_numero{
     public static void main(String[] args){
-        Scanner teclat = new Scanner(System.in);
-        //Llegir numeros array
-        //Array numeros on buscar
-        int[] buscar_aqui= new int[6];
-        int i = 0;
-        // Bucle llegix 6 numeros
-        while(i<6){
-          // Llegir numero
-          buscar_aqui[i]=teclat.nextInt();
-          //Compta voltes al Bucle
-          i++;
-          // Si el numero buscat es troba en els llegits
-        }
-        int num_buscat = teclat.nextInt();
-        for (i=0; i<buscar_aqui[i]; i++) {
-          if(num_buscat==buscar_aqui[i]){
-            System.out.println("Has acabat! L'has trobat en la posicio: " + (i)); 
-            break;
-          } else if (i==5){
-            System.out.println("No s'ha trobat!");
-            break;
+        try (Scanner teclat = new Scanner(System.in)) {
+          //Llegir numeros array
+          //Array numeros on buscar
+          int[] buscar_aqui= new int[6];
+          int i = 0;
+          // Bucle llegix 6 numeros
+          while(i<6){
+            // Llegir numero
+            buscar_aqui[i]=teclat.nextInt();
+            //Compta voltes al Bucle
+            i++;
+            // Si el numero buscat es troba en els llegits
+          }
+          int num_buscat = teclat.nextInt();
+          for (i=0; i<buscar_aqui[i]; i++) {
+            if(num_buscat==buscar_aqui[i]){
+              System.out.println("Has acabat! L'has trobat en la posicio: " + (i)); 
+              break;
+            } else if (i==5){
+              System.out.println("No s'ha trobat!");
+              break;
+            }
           }
         }
     }
--- array_ncastell/calse26gener/array_comparar.java
+++ array_ncastell/calse26gener/array_comparar.java
@@ -8,33 +8,34 @@ import java.util.Scanner;
 //Imprimeix el nombre d'elements que són iguals al darrer, exclòs aquest.
 public class array_comparar{
     public static void main(String[] args){
-      Scanner entrada = new Scanner(System.in);
-      int numero_vegades= 0;
-      int i= 0;
-      int j= 0;
-      int s= 0;
-      //tipo [] nombre-array= tipo nombre-array[];
-      int []buscar_aqui= new int[i];
-      //nombre natural n> 0
-      numero_vegades= entrada.nextInt();
-      //Si el nombre es menor a 0
-      if(numero_vegades<0){
-        //llegim les dades tantes vegades com sigui necessari
-        while(numero_vegades<=j){
-        j++;
-        //llegim i guardem a l'array
-        buscar_aqui[i]=entrada.nextInt();      
+      try (Scanner entrada = new Scanner(System.in)) {
+        int numero_vegades= 0;
+        int i= 0;
+        int j= 0;
+        int s= 0;
+        //tipo [] nombre-array= tipo nombre-array[];
+        int []buscar_aqui= new int[i];
+        //nombre natural n> 0
+        numero_vegades= entrada.nextInt();
+        //Si el nombre es menor a 0
+        if(numero_vegades<0){
+          //llegim les dades tantes vegades com sigui necessari
+          while(numero_vegades<=j){
+          j++;
+          //llegim i guardem a l'array
+          buscar_aqui[i]=entrada.nextInt();      
+          }
         }
-      }
-      //Recorrem l'array
-      for (i=0; i<buscar_aqui[i]; i++){
-        //Comparem la posició i de l'array amb l'ultima posició
-        if(buscar_aqui[i]==buscar_aqui[numero_vegades-1]){
-          //contem quants hi han de repetits
-          s++;
+        //Recorrem l'array
+        for (i=0; i<buscar_aqui[i]; i++){
+          //Comparem la posició i de l'array amb l'ultima posició
+          if(buscar_aqui[i]==buscar_aqui[numero_vegades-1]){
+            //contem quants hi han de repetits
+            s++;
+          }
         }
+        System.out.println("Hem trobat " + s + " vegades el ultim nombre introduit" ); 
+        entrada.close();
       }
-      System.out.println("Hem trobat " + s + " vegades el ultim nombre introduit" ); 
-      entrada.close();
   }
 }
