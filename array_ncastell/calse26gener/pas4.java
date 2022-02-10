fc88a1b2e7000897cfe1c0b7c02a8fb86e78cdec
nataliacastell <94447965+nataliacastell@users.noreply.github.com>
2022-02-10 09:27:38 +0100

    projectes java3

--- array_ncastell/calse26gener/pas4.java
+++ array_ncastell/calse26gener/pas4.java
@@ -4,66 +4,67 @@ import java.util.Scanner;
 
 public class pas4{
     public static void main(String[] args){
-        Scanner teclat = new Scanner(System.in);
-        int num_pilots = teclat.nextInt();
-        int entreno = teclat.nextInt();
-        double cent=0;
-            double mes_60=0;
-            double menos_60=0;
-        /* Per guardar el millorPromig. Inicialitzar variables  */
-        String nom_millor = "";
-        double millor_promig = 10000;
+        try (Scanner teclat = new Scanner(System.in)) {
+            int num_pilots = teclat.nextInt();
+            int entreno = teclat.nextInt();
+            double cent=0;
+                double mes_60=0;
+                double menos_60=0;
+            /* Per guardar el millorPromig. Inicialitzar variables  */
+            String nom_millor = "";
+            double millor_promig = 10000;
 
 
-        /* Bucle P vegades processar dades de tots els pilots */
-        int comptador2 = 0;
-        while(comptador2<num_pilots){
-            /* AQUI: Comença COPIA del pas 2 */
-            String nom_pilot = teclat.next();
-            /* bucle per llegir E enters */
-            int sumaTemps = 0;
-            int numEnBO = 0;
-            int comptador= 0;
-            while(comptador<entreno){
-                int tempsentreno = teclat.nextInt();
-                sumaTemps += tempsentreno;    // suma dels temps
-                if(tempsentreno<=10){
-                    numEnBO++;
+            /* Bucle P vegades processar dades de tots els pilots */
+            int comptador2 = 0;
+            while(comptador2<num_pilots){
+                /* AQUI: Comença COPIA del pas 2 */
+                String nom_pilot = teclat.next();
+                /* bucle per llegir E enters */
+                int sumaTemps = 0;
+                int numEnBO = 0;
+                int comptador= 0;
+                while(comptador<entreno){
+                    int tempsentreno = teclat.nextInt();
+                    sumaTemps += tempsentreno;    // suma dels temps
+                    if(tempsentreno<=10){
+                        numEnBO++;
+                    }
+                    comptador++;
                 }
-                comptador++;
-            }
 
-            /* Calcul promig */
-            double tempsPromig = (double)sumaTemps/entreno;
+                /* Calcul promig */
+                double tempsPromig = (double)sumaTemps/entreno;
 
-            /* Calcular percentatge d'entrenos BO */
-            double percent_bo = 100*(double)numEnBO/entreno;
-            /* AQUI: acaba COPIA del pas 2 */
-            /* Buscar millor promig (pas4) */
+                /* Calcular percentatge d'entrenos BO */
+                double percent_bo = 100*(double)numEnBO/entreno;
+                /* AQUI: acaba COPIA del pas 2 */
+                /* Buscar millor promig (pas4) */
 
-            if(millor_promig>tempsPromig){
-                millor_promig=tempsPromig;
-                nom_millor=nom_pilot;
+                if(millor_promig>tempsPromig){
+                    millor_promig=tempsPromig;
+                    nom_millor=nom_pilot;
+                }
+                /*Pas 5 a casa*/
+                
+                /*Comprova quants pilots tenen cert percentatge de entrenos bo */
+                if(percent_bo==100){
+                  cent++;
+                }else if(percent_bo>=60 && percent_bo<100){
+                  mes_60++;
+                }else{
+                  menos_60++;
+                }
+                /*Aqui acaba la part 5*/
+                comptador2++;
             }
-            /*Pas 5 a casa*/
-            
-            /*Comprova quants pilots tenen cert percentatge de entrenos bo */
-            if(percent_bo==100){
-              cent++;
-            }else if(percent_bo>=60 && percent_bo<100){
-              mes_60++;
-            }else{
-              menos_60++;
-            }
-            /*Aqui acaba la part 5*/
-            comptador2++;
+            /* Resultats  */
+            System.out.println("Millor Pilot: " + nom_millor);
+            System.out.println("Millor Promig: " + millor_promig);
+            System.out.println("100% d'entrenos BO: " + cent);
+            System.out.println("Entre el 60% i el 100% d'entrenos BO: " + mes_60);
+            System.out.println("Menys del 60% d'entrenos BO: " + menos_60);
         }
-        /* Resultats  */
-        System.out.println("Millor Pilot: " + nom_millor);
-        System.out.println("Millor Promig: " + millor_promig);
-        System.out.println("100% d'entrenos BO: " + cent);
-        System.out.println("Entre el 60% i el 100% d'entrenos BO: " + mes_60);
-        System.out.println("Menys del 60% d'entrenos BO: " + menos_60);
 
 
 
--- array_ncastell/exercici_elevat/Main.java
+++ array_ncastell/exercici_elevat/Main.java
@@ -6,18 +6,19 @@ import java.util.Scanner;
 //Imprimir el vector original i el resultat.
 public class Main{
     public static void main(String[] args){
-      Scanner entrada = new Scanner(System.in);
-      int x= 7;
-      double [] numeros= new double[x];
-      double [] elevat= new double[x]; 
-      for(int i=0; i<7; i++){
-        numeros[i]= entrada.nextInt();
-        //System.out.println("num natural: " + numeros[i]);
+      try (Scanner entrada = new Scanner(System.in)) {
+        int x= 7;
+        double [] numeros= new double[x];
+        double [] elevat= new double[x]; 
+        for(int i=0; i<7; i++){
+          numeros[i]= entrada.nextInt();
+          //System.out.println("num natural: " + numeros[i]);
 
-      }
-      for(int i=0; i<7; i++){
-        elevat[i]=Math.pow(numeros[i], 2);
-        System.out.println("El numero original es " + numeros[i] +", aquest numero elevat al quadrat es: "+ elevat[i]);
+        }
+        for(int i=0; i<7; i++){
+          elevat[i]=Math.pow(numeros[i], 2);
+          System.out.println("El numero original es " + numeros[i] +", aquest numero elevat al quadrat es: "+ elevat[i]);
+        }
       }
     }
  }
